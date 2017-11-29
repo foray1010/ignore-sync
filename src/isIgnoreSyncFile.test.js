74d305f20775128562df0e71f22787de70136347
foray1010 <foray1010@users.noreply.github.com>
2017-11-29 15:14:27 +0800

    add unit test for isIgnoreSyncFile

---
+++ src/isIgnoreSyncFile.test.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const isIgnoreSyncFile = require('./isIgnoreSyncFile')
+
+describe('isIgnoreSyncFile', () => {
+  test('return true when filename end with ignore-sync', () => {
+    expect(isIgnoreSyncFile('ignore-sync')).toBe(true)
+    expect(isIgnoreSyncFile('.gitignore-sync')).toBe(true)
+    expect(isIgnoreSyncFile('.gitignore-sync2')).toBe(false)
+  })
+
+  test('support relative path', () => {
+    expect(isIgnoreSyncFile('./.gitignore-sync')).toBe(true)
+    expect(isIgnoreSyncFile('../.gitignore-sync')).toBe(true)
+    expect(isIgnoreSyncFile('repo/.gitignore-sync')).toBe(true)
+  })
+
+  test('support absolute path', () => {
+    expect(isIgnoreSyncFile('/.gitignore-sync')).toBe(true)
+    expect(isIgnoreSyncFile('/repo/.gitignore-sync')).toBe(true)
+  })
+})
