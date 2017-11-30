d061e1967a16225e3e8c495772c67fab32784b9d
foray1010 <foray1010@users.noreply.github.com>
2017-11-30 13:22:45 +0800

    add unit test for decodeIgnoreSyncFile

---
+++ src/decodeIgnoreSyncFile.test.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const decodeIgnoreSyncFile = require('./decodeIgnoreSyncFile')
+
+describe('decodeIgnoreSyncFile', () => {
+  test('should generate decoded result', () => {
+    expect(decodeIgnoreSyncFile('[inline]\npattern\npattern2')).toEqual([
+      {source: 'inline', data: ['pattern', 'pattern2']}
+    ])
+  })
+
+  test('support multiple sources', () => {
+    expect(
+      decodeIgnoreSyncFile('[inline]\npattern\npattern2\n[inline]\npattern\npattern2')
+    ).toEqual([
+      {source: 'inline', data: ['pattern', 'pattern2']},
+      {source: 'inline', data: ['pattern', 'pattern2']}
+    ])
+  })
+
+  test('should ignore comments', () => {
+    expect(
+      decodeIgnoreSyncFile('[inline] # comment\n# comment2\npattern\npattern2 # comment 3')
+    ).toEqual([{source: 'inline', data: ['pattern', 'pattern2']}])
+  })
+
+  test('should ignore spaces', () => {
+    expect(decodeIgnoreSyncFile('   [   inline   ]   \n   pattern   \n   pattern2   \n\n')).toEqual(
+      [{source: ' inline ', data: ['pattern', 'pattern2']}]
+    )
+  })
+})
