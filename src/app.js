237ab5919789a452792341af051fd7c37b396c40
foray1010 <foray1010@users.noreply.github.com>
2017-12-05 21:25:22 +0800

    remove development log

--- src/app.js
+++ src/app.js
@@ -13,19 +13,16 @@ module.exports = async () => {
   const projectRoot = await pkgDir()
 
   const ignoreSyncFilePaths = await getIgnoreSyncFiles(projectRoot)
-  console.log(ignoreSyncFilePaths)
 
   const ignoreSyncFiles = await promiseMap(
     readFileFromProjectRoot(projectRoot),
     ignoreSyncFilePaths
   )
-  console.log(JSON.stringify(ignoreSyncFiles, null, 2))
 
   const ignoreFiles = await promiseMap(
     (ignoreSyncFile) => generateIgnoreFile(ignoreSyncFile, projectRoot),
     ignoreSyncFiles
   )
-  console.log(JSON.stringify(ignoreFiles, null, 2))
 
   await dynamicComposeP(
     promiseMap(([relativePath, ignoreFile]) =>
