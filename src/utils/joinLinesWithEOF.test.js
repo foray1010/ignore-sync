d23b84b7f02269f8890f735e782d702c5156e268
foray1010 <foray1010@users.noreply.github.com>
2018-04-26 00:14:58 +0800

    fix: ensure only trailing line break is removed from generated files
    it is possible that trim will remove \r\n from the ignore file which is used by macOS.gitignore

---
+++ src/utils/joinLinesWithEOF.test.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const joinLinesWithEOF = require('./joinLinesWithEOF')
+
+describe('joinLinesWithEOF', () => {
+  test('should join lines with EOF', () => {
+    expect(joinLinesWithEOF(['a', 'b'])).toBe('a\nb\n')
+  })
+
+  test('should ensure only one line break at the end of string', () => {
+    expect(joinLinesWithEOF(['a'])).toBe('a\n')
+    expect(joinLinesWithEOF(['a\n\n'])).toBe('a\n')
+    expect(joinLinesWithEOF(['a\n', '\n'])).toBe('a\n')
+    expect(joinLinesWithEOF(['a', '\n\n'])).toBe('a\n')
+
+    // should not affect inner lines
+    expect(joinLinesWithEOF(['a\n\n', 'b\n\n'])).toBe('a\n\n\nb\n')
+  })
+
+  test('should keep \\r\\n in the end of file for ignoring mac icon\\r\\n', () => {
+    expect(joinLinesWithEOF(['a\r\n'])).toBe('a\r\n')
+  })
+
+  test('should accept empty array', () => {
+    expect(joinLinesWithEOF([])).toBe('\n')
+  })
+})
