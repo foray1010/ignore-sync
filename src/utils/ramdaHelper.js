69226698064c69f23c78873fff6cea59248365d7
Alex Young <foray1010@users.noreply.github.com>
2019-06-11 00:01:56 +0800

    chore: fix eslint error

--- src/utils/ramdaHelper.js
+++ src/utils/ramdaHelper.js
@@ -3,14 +3,18 @@
 const R = require('ramda')
 
 // can compose sync and async functions
-const dynamicComposeP = (...args) => R.composeP(...args, R.bind(Promise.resolve, Promise))
+const dynamicComposeP = (...args) =>
+  R.composeP(
+    ...args,
+    R.bind(Promise.resolve, Promise)
+  )
 exports.dynamicComposeP = dynamicComposeP
+
+const promiseMap = R.curry((fn, array) => Promise.all(R.map(fn, array)))
+exports.promiseMap = promiseMap
 
 const promiseFilter = R.curry(async (fn, array) => {
   const mapped = await promiseMap(fn, array)
   return array.filter((x, index) => mapped[index])
 })
 exports.promiseFilter = promiseFilter
-
-const promiseMap = R.curry((fn, array) => Promise.all(R.map(fn, array)))
-exports.promiseMap = promiseMap
