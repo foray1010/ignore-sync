2642c343f06a002122dbc761f7221a9477060046
Alex Young <foray1010@users.noreply.github.com>
2020-03-23 09:18:48 +0800

    refactor: remove fs-extra

--- src/utils/fsHelper.js
+++ src/utils/fsHelper.js
@@ -1,12 +1,12 @@
 'use strict'
 
-const fs = require('fs-extra')
+const fs = require('fs')
 const path = require('path')
 const R = require('ramda')
 
 const isReadable = async dataPath => {
   try {
-    await fs.access(dataPath, fs.constants.R_OK)
+    await fs.promises.access(dataPath, fs.constants.R_OK)
     return true
   } catch (err) {
     return false
@@ -15,19 +15,19 @@ const isReadable = async dataPath => {
 exports.isReadable = isReadable
 
 const isDirectory = async dataPath => {
-  const stats = await fs.stat(dataPath)
+  const stats = await fs.promises.stat(dataPath)
   return stats.isDirectory()
 }
 exports.isDirectory = isDirectory
 
 const overwriteFile = async (filePath, fileStr) => {
-  await fs.unlink(filePath).catch(() => {})
-  await fs.writeFile(filePath, fileStr)
+  await fs.promises.unlink(filePath).catch(() => {})
+  await fs.promises.writeFile(filePath, fileStr)
 }
 exports.overwriteFile = overwriteFile
 
 const readDir = async absoluteDirPath => {
-  const relativeDataPaths = await fs.readdir(absoluteDirPath)
+  const relativeDataPaths = await fs.promises.readdir(absoluteDirPath)
   return R.map(
     relativeDataPath => path.join(absoluteDirPath, relativeDataPath),
     relativeDataPaths,
@@ -36,7 +36,7 @@ const readDir = async absoluteDirPath => {
 exports.readDir = readDir
 
 const readFile = async filePath => {
-  const fileBuffer = await fs.readFile(filePath)
+  const fileBuffer = await fs.promises.readFile(filePath)
   return String(fileBuffer)
 }
 exports.readFile = readFile
