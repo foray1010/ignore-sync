4c92341a7dc8876a703f6f889c816d6fc3c75c86
Alex Young <foray1010@users.noreply.github.com>
2020-06-23 22:31:40 +0800

    fix: support node 10.13

--- src/utils/fsHelper.js
+++ src/utils/fsHelper.js
@@ -2,10 +2,18 @@
 
 const fs = require('fs')
 const path = require('path')
+const { promisify } = require('util')
+
+const fsAccess = promisify(fs.access)
+const fsStat = promisify(fs.stat)
+const fsUnlink = promisify(fs.unlink)
+const fsWriteFile = promisify(fs.writeFile)
+const fsReaddir = promisify(fs.readdir)
+const fsReadFile = promisify(fs.readFile)
 
 const isReadable = async (dataPath) => {
   try {
-    await fs.promises.access(dataPath, fs.constants.R_OK)
+    await fsAccess(dataPath, fs.constants.R_OK)
     return true
   } catch (err) {
     return false
@@ -14,19 +22,19 @@ const isReadable = async (dataPath) => {
 exports.isReadable = isReadable
 
 const isDirectory = async (dataPath) => {
-  const stats = await fs.promises.stat(dataPath)
+  const stats = await fsStat(dataPath)
   return stats.isDirectory()
 }
 exports.isDirectory = isDirectory
 
 const overwriteFile = async (filePath, fileStr) => {
-  await fs.promises.unlink(filePath).catch(() => {})
-  await fs.promises.writeFile(filePath, fileStr)
+  await fsUnlink(filePath).catch(() => {})
+  await fsWriteFile(filePath, fileStr)
 }
 exports.overwriteFile = overwriteFile
 
 const readDir = async (absoluteDirPath) => {
-  const relativeDataPaths = await fs.promises.readdir(absoluteDirPath)
+  const relativeDataPaths = await fsReaddir(absoluteDirPath)
   return relativeDataPaths.map((relativeDataPath) => {
     return path.join(absoluteDirPath, relativeDataPath)
   })
@@ -34,7 +42,7 @@ const readDir = async (absoluteDirPath) => {
 exports.readDir = readDir
 
 const readFile = async (filePath) => {
-  const fileBuffer = await fs.promises.readFile(filePath)
+  const fileBuffer = await fsReadFile(filePath)
   return String(fileBuffer)
 }
 exports.readFile = readFile
