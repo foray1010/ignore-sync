2a4519a1d0f5b40ad650bba50451115d84b94f24
Alex Young <foray1010@users.noreply.github.com>
2021-02-05 23:22:28 +0800

    feat: support glob in local and relative source tag

--- src/decodeIgnoreSyncFile.test.js
+++ src/decodeIgnoreSyncFile.test.js
@@ -3,16 +3,19 @@
 const decodeIgnoreSyncFile = require('./decodeIgnoreSyncFile')
 
 describe('decodeIgnoreSyncFile', () => {
+  const currentDir = '.'
+
   test('should generate decoded result', () => {
-    expect(decodeIgnoreSyncFile('[inline]\npattern\npattern2')).toEqual([
-      { source: 'inline', data: ['pattern', 'pattern2'] },
-    ])
+    expect(
+      decodeIgnoreSyncFile('[inline]\npattern\npattern2', currentDir),
+    ).toEqual([{ source: 'inline', data: ['pattern', 'pattern2'] }])
   })
 
   test('support multiple sources', () => {
     expect(
       decodeIgnoreSyncFile(
         '[inline]\npattern\npattern2\n[inline]\npattern\npattern2',
+        currentDir,
       ),
     ).toEqual([
       { source: 'inline', data: ['pattern', 'pattern2'] },
@@ -20,31 +23,90 @@ describe('decodeIgnoreSyncFile', () => {
     ])
   })
 
+  test.each(['local', 'relative'])(
+    'support glob in %s source tag',
+    (source) => {
+      expect(
+        decodeIgnoreSyncFile(
+          `[${source}]\n__fixtures__/test_glob*.txt`,
+          currentDir,
+        ),
+      ).toEqual([
+        {
+          source,
+          data: ['__fixtures__/test_glob1.txt', '__fixtures__/test_glob2.txt'],
+        },
+      ])
+
+      // absolute path
+      expect(
+        decodeIgnoreSyncFile(
+          `[${source}]\n/__fixtures__/test_glob*.txt`,
+          currentDir,
+        ),
+      ).toEqual([
+        {
+          source,
+          data: ['__fixtures__/test_glob1.txt', '__fixtures__/test_glob2.txt'],
+        },
+      ])
+
+      expect(
+        decodeIgnoreSyncFile(
+          `[${source}]\n__fixtures__/*test_dotfile*.txt`,
+          currentDir,
+        ),
+      ).toEqual([{ source, data: ['__fixtures__/.test_dotfile.txt'] }])
+
+      expect(
+        decodeIgnoreSyncFile(
+          `[${source}]\n__fixtures__/test_nested/**`,
+          currentDir,
+        ),
+      ).toEqual([
+        {
+          source,
+          data: [
+            '__fixtures__/test_nested/level1.txt',
+            '__fixtures__/test_nested/nested/level2.txt',
+          ],
+        },
+      ])
+    },
+  )
+
   test('should throw error if no source', () => {
     const error = new Error(
       'source `[]` not found before ignore pattern is found',
     )
-    expect(() => decodeIgnoreSyncFile('pattern')).toThrow(error)
-    expect(() => decodeIgnoreSyncFile('pattern\n[inline]\npattern')).toThrow(
-      error,
-    )
-    expect(() => decodeIgnoreSyncFile(' [inline]\npattern')).toThrow(error)
+    expect(() => decodeIgnoreSyncFile('pattern', currentDir)).toThrow(error)
+    expect(() =>
+      decodeIgnoreSyncFile('pattern\n[inline]\npattern', currentDir),
+    ).toThrow(error)
+    expect(() =>
+      decodeIgnoreSyncFile(' [inline]\npattern', currentDir),
+    ).toThrow(error)
 
     // allow empty source
-    expect(() => decodeIgnoreSyncFile('[]\npattern')).not.toThrow(error)
+    expect(() => decodeIgnoreSyncFile('[]\npattern', currentDir)).not.toThrow(
+      error,
+    )
     expect(() =>
-      decodeIgnoreSyncFile('[inline]\npattern\n[]\npattern'),
+      decodeIgnoreSyncFile('[inline]\npattern\n[]\npattern', currentDir),
     ).not.toThrow(error)
     expect(() =>
-      decodeIgnoreSyncFile('[]\npattern\n[inline]\npattern'),
+      decodeIgnoreSyncFile('[]\npattern\n[inline]\npattern', currentDir),
     ).not.toThrow(error)
-    expect(() => decodeIgnoreSyncFile('[inline] \npattern')).not.toThrow(error)
+    expect(() =>
+      decodeIgnoreSyncFile('[inline] \npattern', currentDir),
+    ).not.toThrow(error)
   })
 
   test('should ignore comments', () => {
     expect(
       decodeIgnoreSyncFile(
         '[inline] # comment\n# comment2\npattern\npattern2 # comment 3',
+        currentDir,
       ),
     ).toEqual([{ source: 'inline', data: ['pattern', 'pattern2'] }])
   })
