65eaf06d25b3e6fbd643c82425d59a4b7a722cad
Alex Young <foray1010@users.noreply.github.com>
2021-03-15 11:17:13 +0800

    chore: do not use jsdom when testing

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ const config = {
   collectCoverage: true,
   collectCoverageFrom: ['<rootDir>/src/**/*.{cjs,js,mjs,ts,tsx}'],
   coverageReporters: ['lcov', 'text-summary'],
+  testEnvironment: 'node',
   testMatch: ['**/*.{spec,test}.{cjs,js,mjs,ts,tsx}'],
 }
 
